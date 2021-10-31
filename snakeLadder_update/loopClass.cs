c0e6ffac34c5861c4ea513798da28012a35cb865
aakashpotdar <potdaraakash@gmail.com>
2021-10-31 13:08:14 +0530

    Add project files.

---
+++ snakeLadder_update/loopClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snakeLadder_update
+{
+    public class loopClass
+    {
+        public int loopMethod(int nextMove1,int dai)
+        {
+            int position=0;
+            switch (nextMove1)
+            {
+                case 0://no Move
+                    break;
+                case 1://ladder
+                    position = position + dai;
+                    if (position > 100)
+                    {
+                        position = position - dai;
+                    }
+                    break;
+                case 2://snake
+                    position = position - dai;
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+                    break;
+            }
+            return position;
+        }
+    }
+}
